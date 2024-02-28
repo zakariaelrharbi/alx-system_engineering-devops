40fc2c1d21709e6b49dcc6f6bece0403ddd30ed0
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-02-28 23:51:46 +0100

    error fixed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,8 @@
-# puppet declarative script to install flask from pip3.
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Using Puppet, install flask from pip3.
+
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
+  path    => '/usr/bin',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
